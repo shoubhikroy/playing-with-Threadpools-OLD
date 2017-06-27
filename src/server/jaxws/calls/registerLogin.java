0e3c7f253dc6e6ba0e542627c517bd5ab2fb551f
x <x>
2017-06-27 15:33:17 -0400

    DEK

--- src/server/jaxws/calls/registerLogin.java
+++ src/server/jaxws/calls/registerLogin.java
@@ -2,6 +2,8 @@ package server.jaxws.calls;
 
 import server.jaxws.beans.RegisterLoginInfo;
 import server.jaxws.beans.RegisterLoginResult;
+import server.model.User;
+import server.model.UserDao;
 
 import java.util.concurrent.Callable;
 
@@ -18,6 +20,32 @@ public class registerLogin implements Callable<RegisterLoginResult>
     @Override
     public RegisterLoginResult call() throws Exception
     {
-        return null;
+        String fcmkey = input.getKey();
+        String username = input.getUserName();
+        String password = input.getPassword();
+
+        RegisterLoginResult rlr = new RegisterLoginResult();
+        rlr.setKey(fcmkey);
+        rlr.setSuccessFlag(false);
+
+        //check if user exists
+        UserDao userDao = UserDao.getInstance();
+        User user = userDao.getUserByUsernamePassword(username, password);
+        //if new user
+        if (null == user)
+        {
+            //create user
+            user = userDao.createUser(username, password, fcmkey, 0, 0);
+            rlr.setSuccessFlag(true);
+        }
+        //if user exists
+        else
+        {
+            //update key, and return success
+            user.setFcmkey(fcmkey);
+            user.save();
+            rlr.setSuccessFlag(true);
+        }
+        return rlr;
     }
 }
